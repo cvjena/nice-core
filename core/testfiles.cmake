de27476afbaf74b3cfdadb85d4217a9014cb5a1b
Johannes R <jojojena@gmail.com>
2013-03-08 21:58:20 +0100

    enable some basic unit tests

--- core/testfiles.cmake
+++ core/testfiles.cmake
@@ -15,28 +15,28 @@ SET(NICE_TESTFILES_SRC
 # ./image/tests/TestColor.cpp
 # ./image/tests/MultiChannelImageTTest.cpp
 # ./image/tests/TestFourier.cpp
-# ./basics/tests/cppunitexTest.cpp
-# ./basics/tests/FileNameTest.cpp
-# ./basics/tests/Testcasts.cpp
-# ./basics/tests/TestObserver.cpp
-# ./basics/tests/numerictoolsTest.cpp
-# ./basics/tests/TimerTest.cpp
-# ./basics/tests/stringutilsTest.cpp
-# ./basics/tests/Testgzbinstream.cpp
-# ./basics/tests/Testbinstream.cpp
+./basics/tests/cppunitexTest.cpp
+./basics/tests/FileNameTest.cpp
+./basics/tests/Testcasts.cpp
+./basics/tests/TestObserver.cpp
+./basics/tests/numerictoolsTest.cpp
+./basics/tests/TimerTest.cpp
+./basics/tests/stringutilsTest.cpp
+./basics/tests/Testgzbinstream.cpp
+./basics/tests/Testbinstream.cpp
 # ./optimization/gradientBased/tests/TestTrustRegion.cpp
 # ./optimization/blackbox/tests/TestDownhillSimplex.cpp
 # ./algebra/tests/TestLUDecomposition.cpp
 # ./algebra/tests/TestLinearSolve.cpp
 # ./algebra/tests/TestDiagApprox.cpp
 # ./algebra/tests/TestEigenValue.cpp
-# ./vector/tests/TestSparseVector.cpp
-# ./vector/tests/TestEVector.cpp
+./vector/tests/TestSparseVector.cpp
+./vector/tests/TestEVector.cpp
 ./vector/tests/TestDistance.cpp
-# ./vector/tests/TestAlgorithms.cpp
-# ./vector/tests/TestStreamable.cpp
-# ./vector/tests/TestERowMatrix.cpp
-# ./vector/tests/TestEMatrix.cpp
+./vector/tests/TestAlgorithms.cpp
+./vector/tests/TestStreamable.cpp
+./vector/tests/TestERowMatrix.cpp
+./vector/tests/TestEMatrix.cpp
 )
 
 SET(NICE_TESTFILES_HDR
@@ -55,26 +55,26 @@ SET(NICE_TESTFILES_HDR
 # ./image/tests/TestFilter.h
 # ./image/tests/TestImageFileList.h
 # ./image/tests/TestImageFile.h
-# ./basics/tests/TimerTest.h
-# ./basics/tests/Testcasts.h
-# ./basics/tests/numerictoolsTest.h
-# ./basics/tests/Testbinstream.h
-# ./basics/tests/cppunitexTest.h
-# ./basics/tests/Testgzbinstream.h
-# ./basics/tests/FileNameTest.h
-# ./basics/tests/stringutilsTest.h
-# ./basics/tests/TestObserver.h
+./basics/tests/TimerTest.h
+./basics/tests/Testcasts.h
+./basics/tests/numerictoolsTest.h
+./basics/tests/Testbinstream.h
+./basics/tests/cppunitexTest.h
+./basics/tests/Testgzbinstream.h
+./basics/tests/FileNameTest.h
+./basics/tests/stringutilsTest.h
+./basics/tests/TestObserver.h
 # ./optimization/gradientBased/tests/TestTrustRegion.h
 # ./optimization/blackbox/tests/TestDownhillSimplex.h
 # ./algebra/tests/TestDiagApprox.h
 # ./algebra/tests/TestLUDecomposition.h
 # ./algebra/tests/TestEigenValue.h
 # ./algebra/tests/TestLinearSolve.h
-# ./vector/tests/TestStreamable.h
-# ./vector/tests/TestERowMatrix.h
-# ./vector/tests/TestEMatrix.h
-# ./vector/tests/TestAlgorithms.h
-# ./vector/tests/TestEVector.h
+./vector/tests/TestStreamable.h
+./vector/tests/TestERowMatrix.h
+./vector/tests/TestEMatrix.h
+./vector/tests/TestAlgorithms.h
+./vector/tests/TestEVector.h
 ./vector/tests/TestDistance.h
-# ./vector/tests/TestSparseVector.h
+./vector/tests/TestSparseVector.h
 )
