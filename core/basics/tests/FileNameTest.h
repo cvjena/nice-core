6bb7e00fa383bc60dbe311b67122b750d8753ed8
Johannes Ruehle <johannes.ruehle@uni-jena.de>
2014-03-14 10:36:06 +0100

    added check for relative pathes in class FileName

--- core/basics/tests/FileNameTest.h
+++ core/basics/tests/FileNameTest.h
@@ -12,6 +12,7 @@ class FileNameTest : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE( FileNameTest );
   CPPUNIT_TEST( testFileName );
   CPPUNIT_TEST( testFileNameSlash );
+  CPPUNIT_TEST( testPathRelative );
   CPPUNIT_TEST_SUITE_END();
   
  private:
@@ -29,6 +30,11 @@ class FileNameTest : public CppUnit::TestFixture {
    * Test FileName addSlash/removeSlash
    */
   void testFileNameSlash();
+
+  /**
+   * Test FileName for relative and absolute path recognition
+   */
+  void testPathRelative();
 
 };
 
