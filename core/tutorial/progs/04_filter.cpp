f412a8210ba1a08975aba3f23b936929cf1921c9
Clemens-Alexander Brust <ikosa.de@gmail.com>
2014-04-05 16:23:16 +0200

    Added tutorial 04 document and sample code

--- core/tutorial/progs/04_filter.cpp
+++ core/tutorial/progs/04_filter.cpp
@@ -8,9 +8,46 @@
  * @author Clemens-A. Brust
  */
 
+#include <iostream>
+#include <string>
+#include <core/image/ImageT.h>
+#include <core/vector/MatrixT.h>
+#include <core/image/FilterT.h>
+
 /*
  * Entry point
  */
 int main(int argc, char** argv) {
-	return 0;
+	// Check if enough parameters were supplied
+		if (argc < 3) {
+			std::cout << "USAGE: " << argv[0] << " <input image> <output image>\n";
+			return -1;
+		}
+
+		// These are our file names
+		std::string input_path(argv[1]);
+		std::string output_path(argv[2]);
+
+		// Read file header and display header information
+		NICE::ImageFile source_file(input_path);
+
+		// Read image into memory
+		NICE::Image image;
+		source_file.reader(&image);
+
+		// Allocate memory for result image
+		NICE::Image result(image.width(), image.height());
+
+		// Put together a simple convolution kernel for our motion blur effect
+		NICE::Matrix kernel(10,10);
+		kernel.set(0);
+		kernel.addIdentity(0.1);
+
+		// Filter the image
+		NICE::Filter::filter(image, kernel, result);
+
+		// Write image to disk
+		NICE::ImageFile dest_image(output_path);
+		dest_image.writer(&result);
+		return 0;
 }
